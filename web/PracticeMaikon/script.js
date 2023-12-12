47e3bf82fd6d593dd4b531ba50e967bc42099612
Song <mantourm@gmail.com>
2023-12-13 01:36:06 +0800

    Update Maikon JS

--- web/PracticeMaikon/script.js
+++ web/PracticeMaikon/script.js
@@ -2,7 +2,7 @@ window.addEventListener('scroll', function () {
     var nav = document.querySelector('.navSection');
     var scrollPosition = window.scrollY;
 
-    if (scrollPosition > 1) { // 這裡的數值可以根據實際情況調整
+    if (scrollPosition > 1) {
         nav.classList.add('shadow');
     } else {
         nav.classList.remove('shadow');
