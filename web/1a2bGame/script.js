e8819f233be5fc06c35c782a2675c4fe4e77aeb3
Song <mantourm@gmail.com>
2023-12-31 02:56:48 +0800

    Update 1a2bGame

--- web/1a2bGame/script.js
+++ web/1a2bGame/script.js
@@ -4,7 +4,10 @@ const quitBtn = document.getElementById('quitBtn')
 const inputBox = document.getElementById('inputBox')
 const guessBtn = document.getElementById('guessBtn')
 const recordDiv = document.getElementById('recordDiv')
-const answerArr = []
+let answerArr = []
+let inputNum
+let ACount = 0
+let BCount = 0
 
 function createRandomNumber() {
 	while (answerArr.length < 4) {
@@ -31,13 +34,15 @@ startBtn.addEventListener('click', disableStartBtn)
 startBtn.addEventListener('click', enableRestartBtn)
 
 function checkInput() {
-	let inputNum = inputBox.value.replace(/\s/g, '')
+	inputNum = inputBox.value.replace(/\s/g, '')
 	console.log(inputNum)
+
+	let isValid = true
 
 	function checkIfValid(inputNum) {
 		if (inputNum.length != 4 || isNaN(inputNum)) {
 			alert('Please enter a non-repeating 4 digits number')
-			return false
+			return isValid = false
 		}
 
 		function checkIfNumRepeated(inputNum) {
@@ -48,14 +53,12 @@ function checkInput() {
 
 		if (checkIfNumRepeated(inputNum)) {
 			alert('The entered number is repeated')
-			return false
+			return isValid = false
 		}
-		return true
+		return isValid = true
 	}
 
 	if (checkIfValid(inputNum)) {
-		let ACount = 0
-		let BCount = 0
 		let inputArr = inputNum.split('')
 		console.log(answerArr)
 		inputArr.forEach((item, index) => {
@@ -66,18 +69,71 @@ function checkInput() {
 				BCount++
 			}
 		})
-		console.log(`${ACount}A${BCount}B`)
 	}
-
 	inputBox.value = ''
+	console.log(`?A?B : ${ACount}A${BCount}B`)
+	console.log(`inputNum : ${inputNum}`)
+	return [`${ACount}A${BCount}B`, inputNum, isValid]
 }
 
+function AddRecord() {
+	let resultArr = checkInput()
+	console.log(`resultArr : ${resultArr}`)
+
+	let li = document.createElement('li')
+	let span = document.createElement('span')
+	let ul = document.querySelector('ul')
+
+	ul.classList.add('list-group', 'mt-3')
+	li.classList.add('list-group-item', 'position-relative', 'd-flex', 'justify-content-center', 'align-items-center')
+	span.classList.add('position-absolute', 'text-bg-warning', 'px-3', 'py-1', 'start-0', 'ms-2', 'rounded')
+
+
+	if (resultArr[2]) {
+		span.innerText = resultArr[0]
+		li.innerText = resultArr[1]
+
+		if (ACount === 4 && BCount === 0) {
+			span.classList.replace('text-bg-warning', 'text-bg-success')
+		}
+	}
+	else {
+		span.innerText = 'Invalid Input'
+		li.innerText = resultArr[1]
+		span.classList.replace('text-bg-warning', 'text-bg-danger')
+	}
+
+	li.insertBefore(span, li.firstChild)
+	ul.append(li)
+
+	if(span.classList.contains('text-bg-success')){
+		let liSuccess = li.cloneNode(false)
+		liSuccess.classList.add('text-bg-success')
+		liSuccess.innerText = 'Success !'
+		ul.append(liSuccess)
+		quitBtn.setAttribute('disabled', 'true')
+		guessBtn.setAttribute('disabled', 'true')
+	}
+
+	ACount = 0
+	BCount = 0
+}
 
 inputBox.addEventListener('keydown', (e) => {
 	if (e.key === "Enter") {
-		checkInput()
+		AddRecord()
 	}
 })
 
-guessBtn.addEventListener('click', checkInput)
+guessBtn.addEventListener('click', () => {
+	AddRecord()
+})
 
+restartBtn.addEventListener('click', () => {
+	quitBtn.removeAttribute('disabled')
+	guessBtn.removeAttribute('disabled')
+	answerArr = []
+	createRandomNumber()
+	let ul = document.querySelector('ul')
+	ul.innerText = ''
+})
