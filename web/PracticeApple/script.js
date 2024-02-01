40d0487c516993cb2d363e37bac773aedb2694b7
Song <mantourm@gmail.com>
2024-02-02 02:59:21 +0800

    Update Apple

--- web/PracticeApple/script.js
+++ web/PracticeApple/script.js
@@ -1,7 +1,11 @@
-https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/iphone-15-pro-finish-select-202309-6-1inch-naturaltitanium?wid=2560&hei=1440&fmt=p-jpg&qlt=80&.v=1692846363993
 
-https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/iphone-15-pro-finish-select-202309-6-1inch-naturaltitanium_AV2?wid=2560&hei=1440&fmt=p-jpg&qlt=80&.v=1692846363675
+window.onload = function () {
 
-https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/iphone-15-pro-finish-select-202309-6-1inch-naturaltitanium_AV1?wid=2560&hei=1440&fmt=p-jpg&qlt=80&.v=1692846364503
+}
 
-https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/iphone-15-pro-finish-select-202309-6-1inch-naturaltitanium_AV3?wid=2560&hei=1440&fmt=p-jpg&qlt=80&.v=1693011636437
+let iphoneSpecJson = fetch('./iphoneSpec.json')
+	.then(res => {return res.json()})
+	.catch(error => {
+		console.error('Error fetching JSON:', error);
+	});
+	console.log(iphoneSpecJson)
