04e9385093ae7c90c6a91436409b37d4b16f0895
Song <mantourm@gmail.com>
2024-05-13 02:22:20 +0800

    update calendarvue v1

--- web/CalendarVue/calendarScript.js
+++ web/CalendarVue/calendarScript.js
@@ -1,7 +1,8 @@
 const app = Vue.createApp({
 	data() {
 		return {
-			currentDate: new Date(),
+			// 把現在的Date時間部份設為0:00:00:000
+			currentDate: new Date(new Date().setHours(0, 0, 0, 0)),
 			yearAndMonth: '',
 			days: [
 				// 結構如下
@@ -34,11 +35,85 @@ const app = Vue.createApp({
 		}
 	},
 	methods: {
+		getYearAndMonthTitle() {
+			this.yearAndMonth = `${this.currentDate.getFullYear()} - ${String(this.currentDate.getMonth() + 1).padStart(2, 0)}`
+		},
+		getDayBlockData() {
+
+			// 拿到本月1號是星期幾
+			let startingDateOfMonth = new Date(new Date(this.currentDate).setDate(1))
+			console.log(`startingDateOfMonth : ${startingDateOfMonth}`)
+			let startingDayOfMonth = startingDateOfMonth.getDay()
+
+			// 計算出第一個格子的日期，並用於後面的while迴圈持續加一天
+			let startingDateOfCalendar =
+				new Date(new Date(startingDateOfMonth).setDate(1 - startingDayOfMonth))
+			console.log(`this.currentDate : ${this.currentDate}`)
+			console.log(`startingDateOfCalendar : ${startingDateOfCalendar}`)
+
+			// 拿到這個月的最後一天
+			let lastDateOfCurrentMonth = new Date(this.currentDate.getFullYear(), this.currentDate.getMonth() + 1, 0)
+
+			// 填入days陣列的內容
+			// 當月份大於這個月且陣列長度可以被7整除時離開迴圈
+			while (!(startingDateOfCalendar > lastDateOfCurrentMonth && this.days.length % 7 === 0)) {
+
+				// 設定id為yyyyMMdd
+				idString = `${startingDateOfCalendar.getFullYear()}${String(startingDateOfCalendar.getMonth() + 1).padStart(2, 0)}${String(startingDateOfCalendar.getDate()).padStart(2, 0)}`
+
+				// 判斷block是不是這個月的，設定相應的class
+				let className = ''
+				if (startingDateOfCalendar.getMonth() === this.currentDate.getMonth()) {
+					className = 'this-month-block'
+				} else {
+					className = 'not-this-month-block'
+				}
+
+				let todos = []
+				if (this.allTodos.length !== 0) {
+					for (const todoData of this.allTodos) {
+						if (todoData.id == idString) {
+							todos = todoData.todos
+						}
+					}
+				}
+
+				// 產生days的內容
+				this.days.push({
+					id: idString,
+					dayDate: startingDateOfCalendar.getDate(),
+					class: className,
+					todos: todos,
+				})
+
+				// 將日期+1。繼續跑迴圈
+				startingDateOfCalendar.setDate(startingDateOfCalendar.getDate() + 1)
+			}
+
+			console.log('this.days :')
+			console.log(this.days)
+		},
+		prevMonth() {
+			this.currentDate.setMonth(this.currentDate.getMonth() - 1)
+
+			this.reRender()
+			this.getYearAndMonthTitle()
+		},
+		nextMonth() {
+			this.currentDate.setMonth(this.currentDate.getMonth() + 1)
+
+			this.reRender()
+			this.getYearAndMonthTitle()
+		},
 		saveToLocalStorage() {
 			localStorage.setItem('daysInLocalStorage', JSON.stringify(this.allTodos))
 		},
 		loadFromLocalStorage() {
 			return JSON.parse(localStorage.getItem('daysInLocalStorage'))
+		},
+		reRender() {
+			this.days = []
+			this.getDayBlockData()
 		},
 		// 拿到被點擊的day-block的id，下面addTodo()要用的
 		openModalForAddingTodo(input) {
@@ -89,9 +164,7 @@ const app = Vue.createApp({
 
 			this.saveToLocalStorage()
 
-			// 清空格子重新render
-			this.days = []
-			this.getDayBlockData
+			this.reRender()
 		},
 		openModalForUpdatingTodo(e, todo, day) {
 			// 防止冒泡出去，會同時呼叫openModalForAddingTodo(input)那個方法
@@ -122,7 +195,7 @@ const app = Vue.createApp({
 		},
 		updateTodo() {
 			console.log('updateTodo()')
-			
+
 
 			// 找到對應的那筆todo
 			let editingTodo = (this.allTodos.find(todo => todo.id === this.todoInModalId)).todos.find(todo => todo.time == this.objectToEdit.time && todo.content == this.objectToEdit.content)
@@ -135,10 +208,7 @@ const app = Vue.createApp({
 
 			this.saveToLocalStorage()
 
-			// 清空格子重新render
-			this.days = []
-			this.getDayBlockData
-
+			this.reRender()
 		},
 		deleteTodo() {
 			console.log('deleteTodo()')
@@ -152,64 +222,16 @@ const app = Vue.createApp({
 
 			this.saveToLocalStorage()
 
-			// 清空格子重新render
-			this.days = []
-			this.getDayBlockData
+			this.reRender()
 		}
 	},
 	computed: {
-		getYearAndMonthTitle() {
-			return `${this.currentDate.getFullYear()} - ${String(this.currentDate.getMonth() + 1).padStart(2, 0)}`
-		},
-		getDayBlockData() {
-			// 拿到本月1號是星期幾
-			let startingDateOfMonth = new Date(new Date(this.currentDate).setDate(1))
-			let startingDayOfMonth = startingDateOfMonth.getDay()
-			// 計算出第一個格子的日期
-			let startingDateOfCalendar =
-				new Date(new Date(startingDateOfMonth).setDate(1 - startingDayOfMonth))
 
-			// 填入days陣列的內容
-			// 當月份大於這個月且陣列長度可以被7整除時離開迴圈
-			while (!(startingDateOfCalendar.getMonth() > this.currentDate.getMonth() && this.days.length % 7 === 0)) {
-
-				// 設定id為yyyyMMdd
-				idString = `${startingDateOfCalendar.getFullYear()}${String(startingDateOfCalendar.getMonth() + 1).padStart(2, 0)}${String(startingDateOfCalendar.getDate()).padStart(2, 0)}`
-
-				// 判斷block是不是這個月的，設定相應的class
-				let className = ''
-				if (startingDateOfCalendar.getMonth() === this.currentDate.getMonth()) {
-					className = 'this-month-block'
-				} else {
-					className = 'not-this-month-block'
-				}
-
-				let todos = []
-				if (this.allTodos.length !== 0) {
-					for (const todoData of this.allTodos) {
-						if (todoData.id == idString) {
-							todos = todoData.todos
-						}
-					}
-				}
-
-				// 產生days的內容
-				this.days.push({
-					id: idString,
-					dayDate: startingDateOfCalendar.getDate(),
-					class: className,
-					todos: todos,
-				})
-
-				// 將日期+1。繼續跑迴圈
-				startingDateOfCalendar.setDate(startingDateOfCalendar.getDate() + 1)
-			}
-		}
 	},
 	mounted() {
 		console.log(`this.currentDate : ${this.currentDate}`)
 		// 設定行事曆年份月份標題
-		this.yearAndMonth = this.getYearAndMonthTitle
+		this.getYearAndMonthTitle()
 
 		// 把todos從local storage拿出來放到this.allTodos
 		this.allTodos = this.loadFromLocalStorage() || []
@@ -217,7 +239,7 @@ const app = Vue.createApp({
 		console.log(this.allTodos)
 
 		// 產生每天的block
-		this.getDayBlockData
+		this.getDayBlockData()
 		// 驗證block格式
 		console.log('First item of this.days')
 		console.log(this.days[0])
